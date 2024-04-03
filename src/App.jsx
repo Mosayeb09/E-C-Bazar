31b4c801323e99b873933c4bd6c6b547faf4e548
rjrupom221 <rjrupom221@gmail.com>
2024-04-03 06:34:44 +0000

    instraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h2>
-        test page
-      </h2>
+     I got it. Now you can start your work. Give me work flow and let's work together. 
     </>
   )
 }
